5c1d4bf81684d3e39cfbec38a5823c448b739307
Lucas Borges <lucasborges@agenciamanga.com>
2018-09-13 17:18:08 -0300

    [FIX] seeder de produto

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -12,5 +12,22 @@ class ProductSeeder extends Seeder
     public function run()
     {
         factory(App\Product::class, 30)->create();
+        factory(App\Product::class)->create([
+          'code'  => '123456',
+          'name'  => 'produto 1',
+          'price' => '127.50'
+        ]);
+
+        factory(App\Product::class)->create([
+          'code' => '654321',
+          'name'  => 'produto 2',
+          'price' => '120.50'
+        ]);
+
+        factory(App\Product::class)->create([
+          'code' => '111222',
+          'name'  => 'produto 3',
+          'price' => '150.50'
+        ]);
     }
 }
