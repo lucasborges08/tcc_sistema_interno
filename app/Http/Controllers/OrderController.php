7b1a82a38594b9f7bc0df57a271c4227e8b569d4
Lucas Borges <lucasborges@agenciamanga.com>
2018-09-26 12:06:16 -0300

    [FIX] inclusão de chave do fornecedor

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -10,17 +10,21 @@ class OrderController extends Controller
 {
     public function update(Request $request){    	
     	try{
-	    	$order = Order::where('id', $request->order_id)->first();
+
+          if(env('PROVIDER_KEY') != $request->provider_key){
+            return response('Unauthorized', 400);
+          }
+
+	    	  $order = Order::where('id', $request->order_id)->first();
 	        $event = new Event;
 	        $event->description = $request->description;
 	        $event->order_id = $request->order_id;
 	        $event->save();
 
-
 	        return 'ok';    	
 
     	}catch(\Exception $e){
-    		return $e->getMessage();
+    		return response('Error: ' . $e->getMessage(), 500);
     	}
     }
 }
