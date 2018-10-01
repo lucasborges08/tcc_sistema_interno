fafd7ad7604a2a0770bec7de42bac51ce5379ba8
Lucas <lkssb1@hotmail.com>
2018-09-30 23:00:27 -0300

    [FIX] seeder de produtos

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -10,8 +10,7 @@ class ProductSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        factory(App\Product::class, 30)->create();
+    {        
         factory(App\Product::class)->create([
           'code'  => '123456',
           'name'  => 'produto 1',
@@ -28,6 +27,42 @@ class ProductSeeder extends Seeder
           'code' => '111222',
           'name'  => 'produto 3',
           'price' => '150.50'
+        ]);
+
+        factory(App\Product::class)->create([
+          'code'  => '111333',
+          'name'  => 'produto 4',
+          'price' => '63.50'
+        ]);
+
+        factory(App\Product::class)->create([
+          'code' => '654321',
+          'name'  => 'produto 5',
+          'price' => '135.40'
+        ]);
+
+        factory(App\Product::class)->create([
+          'code' => '777888',
+          'name'  => 'produto 6',
+          'price' => '550.50'
+        ]);
+
+        factory(App\Product::class)->create([
+          'code'  => '888999',
+          'name'  => 'produto 7',
+          'price' => '58.50'
+        ]);
+
+        factory(App\Product::class)->create([
+          'code' => '159786',
+          'name'  => 'produto 8',
+          'price' => '20.40'
+        ]);
+
+        factory(App\Product::class)->create([
+          'code' => '258147',
+          'name'  => 'produto 9',
+          'price' => '56.50'
         ]);
     }
 }
