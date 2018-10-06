14a2091ad0703b7ad28750037f6566a442501125
Lucas <lkssb1@hotmail.com>
2018-10-06 13:35:49 -0300

    [FIX] inclusão de usuário padrão no seeder

--- database/seeds/ClientSeeder.php
+++ database/seeds/ClientSeeder.php
@@ -12,5 +12,8 @@ class ClientSeeder extends Seeder
     public function run()
     {
         factory(App\Client::class, 30)->create();
+        factory(App\Client::class)->create([
+        	'email' => 'teste@teste.com'
+        ]);
     }
 }
